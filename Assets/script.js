64429d07ed9ab066431ccb42d181704bfe34a218
Matt Dudman <mattdudman95@googlemail.com>
2024-01-14 22:12:22 +0000

    Ajax added to get cityWeather and dayjs added in also. Icon for weather via openweathermap also added in.

--- Assets/script.js
+++ Assets/script.js
@@ -11,4 +11,10 @@ $(document).ready(function () {
 function getWeather(city) {
         const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=df8762169cc4e5c500ae5bd0e9fc3a16&units=metric`;
 
-    });
+        $.ajax({
+            url: queryURL,
+            method: 'GET',
+          }).then(function (response) {
+            const cityWeather = response;
+            const currentDate = dayjs().format('YYYY-MM-DD');
+            const iconURL = `https://openweathermap.org/img/w/${cityWeather.weather[0].icon}.png`;
