94b609910caa0481d559584d9fd27c64351b1670
Matt Dudman <mattdudman95@googlemail.com>
2024-01-14 21:18:16 +0000

    .ready Function added in with const variables including searchForm & search Input. I also added the function to get weather data based on city name from API.

---
+++ Assets/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    const apiKey = 'df8762169cc4e5c500ae5bd0e9fc3a16';
+    const searchForm = $('#search-form');
+    const searchInput = $('#search-input');
+    const historyList = $('#history');
+    const todaySection = $('#today');
+    const forecastSection = $('#forecast');
+}
+
+
+function getWeather(city) {
+        const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=df8762169cc4e5c500ae5bd0e9fc3a16&units=metric`;
+
+    });
