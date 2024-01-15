8ca6872bee27be255deec3ed404c1fb77b07d2bb
Matt Dudman <mattdudman95@googlemail.com>
2024-01-15 14:16:36 +0000

    currentWeatherHTML added alongside HTML elements to display current weather in searched loctaion. Also const to get the lat and lon needed for the 5 day forecast now added in.

--- Assets/script.js
+++ Assets/script.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
     const forecastSection = $('#forecast');
 }
 
-
 function getWeather(city) {
         const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=df8762169cc4e5c500ae5bd0e9fc3a16&units=metric`;
 
@@ -18,3 +17,17 @@ function getWeather(city) {
             const cityWeather = response;
             const currentDate = dayjs().format('YYYY-MM-DD');
             const iconURL = `https://openweathermap.org/img/w/${cityWeather.weather[0].icon}.png`;
+
+            const currentWeatherHTML = `
+            <h2>${cityWeather.name} (${currentDate}) <img src="${iconURL}" alt="Weather Icon"></h2>
+            <p>Temperature: ${cityWeather.main.temp} °C</p>
+            <p>Humidity: ${cityWeather.main.humidity}%</p>
+            <p>Wind Speed: ${cityWeather.wind.speed} m/s</p>
+          `;
+          todaySection.html(currentWeatherHTML);
+          
+          const lat = cityWeather.coord.lat;
+          const lon = cityWeather.coord.lon;
+          getForecast(lat, lon);
+        });
+      }
